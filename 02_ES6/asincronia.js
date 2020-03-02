f12b1cae791debf26c00b21cbea8585e19eb02f5
barbara <bgfa26@gmail.com>
2020-03-02 19:27:09 +0100

    asincronia

---
+++ 02_ES6/asincronia.js
@@ -0,0 +1,14 @@
+setTimeout(()=>{
+    console.log(1)}, 0)
+    
+console.log(2)
+
+setTimeout(()=>{
+    console.log(3)}, 1000)
+
+console.log(4)
+
+setTimeout(()=>{
+    console.log(5)}, 2000)
+
+console.log(6)
